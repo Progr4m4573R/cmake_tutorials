27223c8222ae49ec86163e1c4f3758122df53150
Stephen Rerri-Bekibele <ghostye@hotmail.com>
2024-01-04 15:30:06 +0000

    Add files via upload
    updated batch script to clean up more files

--- run_example.bat
+++ run_example.bat
@@ -14,3 +14,22 @@ cmake --build .
 cd Debug
 tutorial %arg2%
 
+REM Now clear all unnecessary files source: https://stackoverflow.com/questions/13139719/delete-all-files-of-specific-type-extension-recursively-down-a-directory-using
+cd ..
+del /S *.vcxproj
+del /S *.vcxproj.filters 
+del /S *.sln
+del TutorialConfig.h
+del CMakeCache.txt
+del cmake_install.cmake
+REM delete folders without prompt source: https://superuser.com/questions/179660/how-to-recursively-delete-directory-from-command-line-in-windows
+rmdir CMakeFiles Debug Tutorial.dir x64 /s /q
+REM also clear sub directory if it exists source: https://stackoverflow.com/questions/21033801/checking-if-a-folder-exists-using-a-bat-file
+if exist MathFunctions\ (
+cd MathFunctions
+rmdir CMakeFiles Debug MathFunctions.dir SqrtLibrary.dir /s /q
+del cmake_install.cmake
+) else (
+    echo no sub folder to delete
+)
+
